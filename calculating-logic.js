7b9ac06014b7a0755f8574ae3e1aaee3e06662ce
Kim <kimkl@home.nl>
2026-02-08 10:58:07 +0100

    First calculations possible
    After entering an expression with two numbers the = button needs to be pressed

--- calculating-logic.js
+++ calculating-logic.js
@@ -1,7 +1,59 @@
-const calculatorDiv = document.querySelector(".calculator-container")
+const btnNodeList = document.querySelectorAll("button");
+const display = document.querySelector(".display");
+let numA = "";
+let operator = "";
+let numB = "";
+let firstNumComplete = false;
 
-for (let i = 0; i < 10; i++) {
-    const button = document.createElement("button");
-    button.textContent = i;
-    calculatorDiv.appendChild(button);
+let opInput = "+-×÷"
+
+
+for (let i = 0; i < btnNodeList.length; i++) {
+
+    btnNodeList[i].onclick = () => {
+        const pressedButton = btnNodeList[i].textContent;
+        display.textContent += pressedButton;
+
+        if (pressedButton === "=") {
+            let total = calculate(numA, numB, operator);
+            display.textContent = total;
+            clearInputs(total);
+        } else if (opInput.includes(pressedButton)) {
+            firstNumComplete = true;
+            operator = pressedButton;
+        } else {
+            // A number was clicked:
+            if (!firstNumComplete) {
+                numA += pressedButton;
+            } else {
+                numB += pressedButton
+            }
+        }
+
+
+        console.log("numA = " + numA);
+        console.log("numB = " + numB);
+        console.log("op = " + operator);
+    };
+}
+
+// Make the calculations
+function calculate(a, b, op) {
+    switch (op) {
+        case "+":
+            return Number(a) + Number(b);
+        case "-":
+            return Number(a) - Number(b);
+        case "×":
+            return Number(a) * Number(b);
+        case "÷":
+            return Number(a) / Number(b);
+    }
+}
+
+// Clear the inputs after a calculation was done
+function clearInputs(prevTotal) {
+    numA = prevTotal;
+    numB = "";
+    operator = "";
 }
